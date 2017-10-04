fbb734b3a5e913c6a4ceaf2355c66731d6652c26
marynganga <marynganga.mw@gmail.com>
2017-10-05 00:42:44 +0300

    Added functionality to the venue list to load more info on clicking the name

--- app/router.js
+++ app/router.js
@@ -14,7 +14,7 @@ Router.map(function() {
   this.route('events', {path: 'events/:params.location'});
   this.route('outdoors',{path:'outdoors/:params.location'});
   this.route('arts',{path:'arts/:params.location'});
-  this.route('view-venue');
+  this.route('view-venue',{path:'view-venue/:params.id'});
 });
 
 export default Router;
