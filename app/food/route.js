555b233df42790430631e8816493888933f0b72c
marynganga <marynganga.mw@gmail.com>
2017-10-04 14:12:10 +0300

    Add a parameter in the get request url to filter results according to category chosen

--- app/food/route.js
+++ app/food/route.js
@@ -8,11 +8,53 @@ export default Ember.Route.extend({
       var CLIENT_ID = config.myCLIENT_ID;
       var CLIENT_SECRET = config.myCLIENT_SECRET;
       var location= Object.values(params);
-      var url = 'https://api.foursquare.com/v2/venues/search?near='+location+'&limit=25&client_id='+CLIENT_ID+'&client_secret='+CLIENT_SECRET+'&v=20171003';
+      var url = 'https://api.foursquare.com/v2/venues/explore?near='+location+'&categoryId=4d4b7105d754a06374d81259&limit=25&client_id='+CLIENT_ID+'&client_secret='+CLIENT_SECRET+'&v=20171003';
    console.log(url);
     return Ember.$.getJSON(url).then(function(responseJSON) {
-
-      return responseJSON.response.venues;
+    	console.log(responseJSON);
+      return responseJSON.response.groups[0].items;
     });
   },
 });
+
+
+// import Ember from 'ember';
+ 
+// export default Ember.Component.extend({
+//   store: Ember.inject.service(),
+//   posts: Ember.computed(function() {
+//     return this.get('store').peekAll('post');
+//   }),
+ 
+//   pageSize: 20,
+//   pageNumber: null,
+//   recordCount: null,
+ 
+//   sortProps: ['createdAt:desc'],
+//   sortedPosts: Ember.computed.sort('posts', 'sortProps'),
+ 
+//   loadPosts: function(getPageNumber) {
+//     const pageSize = this.get('pageSize');
+ 
+//     this.get('store').unloadAll('post');
+//     this.get('store').
+//       query('post', {page: {number: getPageNumber, size: pageSize}}).
+//       then(function(result) {
+//         this.setProperties({
+//         	'recordCount': result.get('meta.record-count'),
+//         	'pageNumber': getPageNumber
+//         });
+//       }.bind(this));
+//   },
+ 
+//   init: function() {
+//     this._super(...arguments);
+//     this.loadPosts(1);
+//   },
+ 
+//   actions: {
+//     getPage: function(getPageNumber) {
+//       this.loadPosts(getPageNumber);
+//     }
+//   }
+// });
