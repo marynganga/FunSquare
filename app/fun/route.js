326e7c4a9dcd021a9ac83c3c1c94fe21d1d7119c
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-10-04 17:16:04 +0300

    Add rating and cost to venues-list.hbs

--- app/fun/route.js
+++ app/fun/route.js
@@ -6,7 +6,7 @@ export default Ember.Route.extend({
     var CLIENT_ID = config.myCLIENT_ID;
     var CLIENT_SECRET = config.myCLIENT_SECRET;
     var location = Object.values(params);
-    var url = 'https://api.foursquare.com/v2/venues/explore?near=' + location + '&categoryId=4d4b7104d754a06370d81259,categoryId=4d4b7105d754a06377d81259&limit=25&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=20171003';
+    var url = 'https://api.foursquare.com/v2/venues/explore?near=' + location + '&categoryId=4d4b7104d754a06370d81259&categoryId=4d4b7105d754a06377d81259&limit=25&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=20171003';
     console.log(url);
     return Ember.$.getJSON(url).then(function(responseJSON) {
       console.log(responseJSON);
