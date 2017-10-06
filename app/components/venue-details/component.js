8171fbbc31beb2af261d9628a137415cad3e7f93
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-10-06 12:24:13 +0300

    Styling

--- app/components/venue-details/component.js
+++ app/components/venue-details/component.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  createEventClicked: null,
+  actions: {
+    createEvent() {
+      this.set('createEventClicked', "I want to add an event.")
+    }
+  }
 });
