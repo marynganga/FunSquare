650c081ddebfd6ff5055b8501fc6483a6c5337ed
marynganga <marynganga.mw@gmail.com>
2017-10-06 13:53:56 +0300

    Merge CSS

--- app/components/venue-details/component.js
+++ app/components/venue-details/component.js
@@ -1,14 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-<<<<<<< HEAD
-
-=======
-  createEventClicked: null,
-  actions: {
-    createEvent() {
-      this.set('createEventClicked', "I want to add an event.")
-    }
-  }
->>>>>>> 8171fbbc31beb2af261d9628a137415cad3e7f93
 });
